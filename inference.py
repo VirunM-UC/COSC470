5eea06be2b5b807233ed558f014d89ab4767d264
VirunM-UC <vvm16@uclive.ac.nz>
2025-05-22 15:52:24 +1200

    run inference on structure_type and print to excel sheet.

--- inference.py
+++ inference.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from transformers import pipeline
 
 def load_data(data_folder, file_name):
     """Takes a pickled dataframe and returns a Tensorflow Dataset
@@ -31,4 +32,21 @@ df = load_data("data/", "validation.pkl")
 
 df = df[~df["structure_type"].isna()] #filter NaNs
 
-df_to_excel(df, "excel_outputs/pandas_image.xlsx")
+
+classifier = pipeline("image-classification", model = "ViT-structure_type-model/checkpoint-78")
+
+structure_type_predict = []
+for i in range(len(df)):
+    image = df.loc[df.index[i], "image"]
+    scores = classifier(image) #result is list of dicts {score:, label:}
+    max_score = max(scores, key = lambda x: x['score'])
+    structure_type_predict.append(max_score['label'])
+
+images = df.loc[:, "image"]
+structure_type_actual = df.loc[:, "structure_type"]
+correct = [structure_type_actual.iloc[i] == structure_type_predict[i] for i in range(len(df))]
+
+df_structure_type = pd.DataFrame({"structure_type_predict": structure_type_predict, "structure_type_actual": structure_type_actual, 
+                                  "correct": correct, "image": images})
+
+df_to_excel(df_structure_type, "excel_outputs/vit_structure_type.xlsx")
