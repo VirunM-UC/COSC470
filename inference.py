b826a008e76ed6062c31369f2c2802b441e681b6
VirunM-UC <vvm16@uclive.ac.nz>
2025-05-27 18:28:42 +1200

    implement inference on building_conditions.

--- inference.py
+++ inference.py
@@ -3,6 +3,9 @@ from transformers import pipeline
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
 
+LABELS = {"structure_type": ("attached", "semi-detached", "detached"),
+          "building_conditions": ("very poor", "poor", "fair", "good", "very good")}
+
 def load_data(data_folder, file_name):
     """Takes a pickled dataframe and returns a Pandas Dataframe
     """
@@ -29,39 +32,44 @@ def df_to_excel(df, file_path):
     # Close the Pandas Excel writer and output the Excel file.
     writer.close()
 
-def main(data_folder, file_path):
+def main(data_folder, file_path, attribute, checkpoint_num):
     df = load_data(data_folder, "validation.pkl")
 
-    df = df[~df["structure_type"].isna()] #filter NaNs
+    df = df[~df[attribute].isna()] #filter NaNs
 
 
-    classifier = pipeline("image-classification", model = "vit-structure_type-model/checkpoint-78")
+    classifier = pipeline("image-classification", model = f"vit-{attribute}-model/checkpoint-{checkpoint_num}")
 
+    #This section would be functionalised with building_conditions as regression.
     scores = classifier(df["image"].to_list()) #result is list of list of dicts {score:, label:}. One dict for each label, one list of dicts for each image.
     max_scores = map(lambda per_image: max(per_image, key = lambda per_label: per_label['score']), scores) #result is list of dicts, each dict being th predicted label of an image
-    structure_type_predict = list(map(lambda per_image: per_image['label'], max_scores))
+    attribute_predict = list(map(lambda per_image: per_image['label'], max_scores))
 
 
     city_name = df.loc[:, "City_Name"]
     images = df.loc[:, "image"]
-    structure_type_actual = df.loc[:, "structure_type"]
-    correct = [structure_type_actual.iloc[i] == structure_type_predict[i] for i in range(len(df))]
+    attribute_actual = df.loc[:, attribute]
+    correct = [attribute_actual.iloc[i] == attribute_predict[i] for i in range(len(df))]
 
-    df_structure_type = pd.DataFrame({"City_Name": city_name, "structure_type_predict": structure_type_predict,
-                                      "structure_type_actual": structure_type_actual, "correct": correct, "image": images})
+    df_attribute = pd.DataFrame({"City_Name": city_name, f"{attribute}_predict": attribute_predict,
+                                      f"{attribute}_actual": attribute_actual, "correct": correct, "image": images})
 
-    df_to_excel(df_structure_type, file_path)
+    df_to_excel(df_attribute, file_path)
 
-    for name in df_structure_type["City_Name"].cat.categories:
-        city_df = df_structure_type.loc[df_structure_type["City_Name"] == name]
+    accuracy = sum(df_attribute["correct"]) / len(df_attribute)
+    print(f"{attribute} - Accuracy: {accuracy:.3f}")
+    for name in df_attribute["City_Name"].cat.categories:
+        city_df = df_attribute.loc[df_attribute["City_Name"] == name]
         accuracy = sum(city_df["correct"]) / len(city_df)
         print(name, f"- Accuracy: {accuracy:.3f}")
 
-    ConfusionMatrixDisplay.from_predictions(structure_type_actual, structure_type_predict, labels = ["attached", "semi-detached", "detached"])
+    ConfusionMatrixDisplay.from_predictions(attribute_actual, attribute_predict, labels = LABELS[attribute])
     plt.show()
 
 
 if __name__ == "__main__":
+    #attribute, checkpoint_num = "structure_type", 78
+    attribute, checkpoint_num = "building_conditions", 75
     data_folder = "data/"
-    file_path = "excel_outputs/vit_structure_type.xlsx"
-    main(data_folder, file_path)
+    file_path = f"excel_outputs/vit_{attribute}.xlsx"
+    main(data_folder, file_path, attribute, checkpoint_num)
